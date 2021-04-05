331a8d0a908269a1cea05c53170bc8adacecc009
himanshu <himanshumirwal@gmail.com>
2021-04-05 17:56:43 +0530

    basic code

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+  const [output, setText] = useState("start typing in text box.")
+  const [outputText, setOytpuText] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 50,textDecorationLine:'underline', marginBottom:20 }}>First React App</Text>
+      <Text style={{ fontSize: 20 }}>{output}</Text>
+      <TextInput value={outputText} onChangeText={(e) => setOytpuText(e)} style={styles.TextInputStyle} />
+      <Button title="Change text" onPress={() => { setText(outputText), setOytpuText("") }} />
     </View>
   );
 }
@@ -18,4 +24,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TextInputStyle: {
+    padding: 10,
+    backgroundColor: "#eee",
+    fontSize: 20,
+    margin: 20,
+    width: 300,
+    borderWidth: 2,
+
+    // Set border Hex Color Code Here.
+    borderColor: 'black',
+
+    // Set border Radius.
+    borderRadius: 20,
+  }
 });
